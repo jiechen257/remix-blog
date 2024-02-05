70b4f2ee47157f86f4a570bee5c7778975e9d627
jiechen <1487503910@qq.com>
2024-02-05 11:10:36 +0800

    feat: add article list and adding articles

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,55 @@
-import type { MetaFunction } from "@remix-run/node";
+import { Pagination } from "@nextui-org/react";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { Link, useLoaderData, useSearchParams } from "@remix-run/react";
+import { prisma } from "~/prisma.server";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+const PAGE_SIZE = 2;
+
+export const loader = async (c: LoaderFunctionArgs) => {
+	const search = new URL(c.request.url).searchParams;
+	const page = Number(search.get("page") || 1);
+
+	const [posts, total] = await prisma.$transaction([
+		prisma.post.findMany({
+			orderBy: {
+				created_at: "desc",
+			},
+			take: PAGE_SIZE,
+			skip: (page - 1) * PAGE_SIZE,
+		}),
+		prisma.post.count(),
+	]);
+	return json({ posts, pageCount: Math.ceil(total / PAGE_SIZE) });
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	const loaderData = useLoaderData<typeof loader>();
+	const [searchParams, setSearchParams] = useSearchParams();
+	const page = Number(searchParams.get("page") || 1);
+
+	return (
+		<div>
+			<div className="flex flex-col gap-4 p-12">
+				{loaderData.posts.map((post) => {
+					return (
+						<div key={post.id}>
+							<Link to={`/posts/${post.id}`} className="text-xl">
+								{post.title}
+							</Link>
+							<div className="text-sm text-gray-400">{new Date(post.created_at).toLocaleString()}</div>
+						</div>
+					);
+				})}
+				<Pagination
+					page={page}
+					total={loaderData.pageCount}
+					onChange={(page) => {
+						const newSearchParams = new URLSearchParams(searchParams);
+						newSearchParams.set("page", String(page));
+						setSearchParams(newSearchParams);
+					}}
+				/>
+			</div>
+		</div>
+	);
 }
